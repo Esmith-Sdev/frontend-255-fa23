64be4bfc6398c72d4a5f305a5c154561d0487eb1
Esmith-Sdev <esmith897@ivytech.edu>
2025-09-22 18:21:02 -0400

    start

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Event listener that will trigger when DOM is loaded (aka upon visiting webpage)
 addEventListener("DOMContentLoaded", async function () {
-    const response = await fetch("http://localhost:3000/api/songs")
+    const response = await fetch("https://backend-255-fa23-5ct7.onrender.com")
     const songs = await response.json()
     let html = ""
     for (let song of songs) {
