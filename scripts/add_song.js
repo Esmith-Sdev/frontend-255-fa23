c4bbe6c99668392d3f45e5b90c0132fba2290e06
Esmith-Sdev <esmith897@ivytech.edu>
2025-10-05 16:02:00 -0400

    initial commit

---
+++ scripts/add_song.js
@@ -0,0 +1,34 @@
+addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#addBtn").addEventListener("click", addSong);
+});
+//add the song to the db.. it has to be asynch function because we are calling data outside our server
+
+async function addSong() {
+  //create a song obj based on the form that the user fills out.
+  const song = {
+    _id: document.querySelector("#songID").value,
+    title: document.querySelector("#title").value,
+    artist: document.querySelector("#artist").value,
+    popularity: document.querySelector("#popularity").value,
+    realeaseDate: document.querySelector("#released").value,
+    genre: document.querySelector("#genre").value
+      ? document.querySelector("#genre").value.split(",")
+      : [],
+  };
+  const response = await fetch("http://localhost:3000/api/songs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(song),
+  });
+
+  if (response.ok) {
+    const results = await response.json();
+    alert("Added song with ID of" + results._id);
+
+    document.querySelector("form").reset();
+  } else {
+    document.querySelector("#error").innerHTML = "Cannot add song";
+  }
+}
